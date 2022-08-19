b4ba69e8c348373da99925c3bc6668672a6717f8
Anastasiia Yavtushenko <Icenn85@gmail.com>
2022-08-19 15:25:47 +0300

    add task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onTimeUpdate, 2000));
+
+function onTimeUpdate(evt) {
+    const time = evt.seconds
+    localStorage.setItem(CURRENT_TIME_KEY, time);
+}
+
+const saveCurrentTime = localStorage.getItem(CURRENT_TIME_KEY);
+
+if (saveCurrentTime) {
+  player.setCurrentTime(saveCurrentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,5 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('feedback-form');
+
+const FEEDBACK_KEY = 'feedback-form-state';
