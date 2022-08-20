dafd68af2c773a5793b472de7e8ca77f7a589009
Anastasiia Yavtushenko <Icenn85@gmail.com>
2022-08-20 10:48:12 +0300

    add task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,43 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('feedback-form');
+const form = document.querySelector('.feedback-form');
 
 const FEEDBACK_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
+
+let formData = {};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    const emailInput = form.elements.email.value;
+    const messageInput = form.elements.message.value;
+    if (emailInput && messageInput !== '') {
+      console.log(JSON.parse(localStorage.getItem(FEEDBACK_KEY)));
+      localStorage.removeItem(FEEDBACK_KEY);
+      evt.currentTarget.reset();
+    } 
+}
+
+
+function onTextareaInput (evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData)); 
+}
+
+savedTextarea();
+function savedTextarea() {
+    const savedMessage = localStorage.getItem(FEEDBACK_KEY);
+
+    if (savedMessage) {
+      savedMessage = JSON.parse(savedMessage);
+      Object.entries(savedMessage).forEach(([name, value]) => {
+        form.elements[name].value = value;
+      });
+    }
+}
+
+
+    
